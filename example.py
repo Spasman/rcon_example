5d5f44792fc75694e3fdfa412f2a3a53bda6e40f
Spasman <spasman@spasmangames.com>
2019-12-30 10:53:55 -0700

    Update example.py
    Added more zombrains RCON events

--- example.py
+++ example.py
@@ -70,6 +70,10 @@ class rcon_event(Enum):
 	zombrains_revive = 50
 	zombrains_buy_weapon = 51
 	zombrains_begin = 52
+	zombrains_helicopter_arriving = 53
+	zombrains_helicopter_boarding = 54
+	zombrains_helicopter_player_boarded = 55
+	zombrains_end = 56
 
 #use these enums when sending requests to your server so the server knows what to do with them
 class rcon_receive(Enum):
